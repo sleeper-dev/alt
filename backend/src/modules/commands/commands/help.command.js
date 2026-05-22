2bc95b2e499a4d43737e8ab015bb747a2d45f4e8
sleeper-dev <darko.p2014@gmail.com>
2026-05-22 03:11:23 +0200

    Show only allowed commands on /help

--- backend/src/modules/commands/commands/help.command.js
+++ backend/src/modules/commands/commands/help.command.js
@@ -3,45 +3,50 @@ export default {
   description: "Shows available commands",
   usage: "/help [command]",
 
-  execute: async ({ socket, args, roomName, commands }) => {
+  execute: async ({ socket, args, commands, room }) => {
+    const canUseCommand = (command) => {
+      if (command.roles?.includes("owner")) {
+        if (!room.ownerId.equals(socket.user._id)) {
+          return false;
+        }
+      }
+      return true;
+    };
+
     if (args.length > 0) {
       const cmdName = args[0].toLowerCase();
       const command = commands.find((item) => item.name === cmdName);
 
-      console.log(command);
-
-      if (!command) {
+      if (!command || !canUseCommand(command)) {
         return socket.emit("system:message", {
-          roomName,
+          roomName: room.name,
           content: `Command "${cmdName}" not found.`,
           createdAt: new Date(),
           system: true,
-          private: true,
         });
       }
 
       return socket.emit("system:message", {
-        roomName,
+        roomName: room.name,
         content:
           `/${command.name}\n` +
           `${command.description}\n` +
           `Usage: ${command.usage}`,
         createdAt: new Date(),
         system: true,
-        private: true,
       });
     }
 
-    const list = Array.from(commands.values())
+    const list = commands
+      .filter(canUseCommand)
       .map((cmd) => `/${cmd.name} — ${cmd.description}`)
       .join("\n");
 
     socket.emit("system:message", {
-      roomName,
+      roomName: room.name,
       content: `Available commands:\n${list}`,
       createdAt: new Date(),
       system: true,
-      private: true,
     });
   },
 };
